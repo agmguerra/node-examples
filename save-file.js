3db8769b342b9f66a9d45c62c2af9980a5591747
AlexandreGuerra <agmguerra@gmail.com>
2020-02-01 14:18:05 -0300

    ajustes

--- save-file.js
+++ save-file.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
+const argv = require('yargs')
+        .alias('f', 'filename')
+        .alias('c', 'content')
+        .demandOtion('filename')
+        .demandOtion('content')
+        .argv
 
-
-fs.writeFile(process.argv[2], process.argv[3], (error)=>{
+fs.writeFile(argv.filename, argv.content, (error)=>{
     if(error) throw error
-    console.log(`Arquivo ${process.argv[2]} foi salvo com sucesso.`)
+    console.log(`Arquivo ${argv.filename} foi salvo com sucesso.`)
 })
